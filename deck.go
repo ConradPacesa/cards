9d5b49eda70d3b87c457b8f04d9e4e9fd73b5bde
Conrad Pacesa <conradpacesa@gmail.com>
2017-09-29 19:35:08 -0400

    Wrote tests

--- deck.go
+++ deck.go
@@ -18,7 +18,8 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six",
+		"Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
